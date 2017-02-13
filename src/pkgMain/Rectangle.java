a417946afe04ad380f5b5fe073e1db06d06ce6e3
天黑请张腿 <@128.175.187.7>
2017-02-12 23:43:38 -0500

    Initial commit

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -4,20 +4,21 @@ public class Rectangle {
 
 	private double Length;
 	private double Width;
+
 	public Rectangle(double length, double width) {
 		Length = length;
 		Width = width;
 	}
+
 	public double getLength() {
 		return Length;
 	}
+
 	public double getWidth() {
 		return Width;
 	}
-	
-	public double Area()
-	{
-		// TODO Implement Area() function with the correct formula
-		return 0;
-	}	
+
+	public double Area() {
+		return getLength() * getWidth();
+	}
 }
